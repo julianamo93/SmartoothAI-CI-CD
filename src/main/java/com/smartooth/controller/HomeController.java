05307dab9931055bd61f6dfb51e33033c30be872
julianamo93 <juliannamor93@gmail.com>
2025-05-21 00:17:47 -0300

    feat: update no properties

--- src/main/java/com/smartooth/controller/HomeController.java
+++ src/main/java/com/smartooth/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Deploy realizado com sucesso!";
+        return "Smartooth AI - Deploy realizado com sucesso!";
     }
 }
